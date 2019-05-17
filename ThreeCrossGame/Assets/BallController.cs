3b3f42f10c54981ca5602acd266f291f43f29f75
Kose-i <tamura.kosei905@mail.kyutech.jp>
2019-05-17 19:21:31 +0900

    Add KeyUI

---
+++ ThreeCrossGame/Assets/BallController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallController : MonoBehaviour {
+
+  float move_under = -1;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    if (transform.position.y > 0) transform.Translate(0, this.move_under, 0);
+	}
+}
--- ThreeCrossGame/Assets/BallGenerator.cs
+++ ThreeCrossGame/Assets/BallGenerator.cs
@@ -6,26 +6,25 @@ public class BallGenerator : MonoBehaviour {
   public string NowTurn = "Black";
   public string NextTurn = "White";
 
-	// Use this for initialization
-	void Start () {
-	}
+  // Use this for initialization
+  void Start () {
+  }
   decimal putSelect() {
-    if (Input.GetMouseButtonDown(0)) {
-      if (Input.mousePosition.x > 0 && Input.mousePosition.y > 0) {
-        return 1;
-      }
+    if (Input.GetKeyDown(KeyCode.Space)) {
+      return 1;
+    } else if (Input.GetKeyDown(KeyCode.RightArrow)) {
+      return 1;
     } else {
       return 0;
     }
-    return 0;
   }
-	
-	// Update is called once per frame
-	void Update () {
+
+  // Update is called once per frame
+  void Update () {
     if (putSelect() == 1) {
       string tmpTurn = NextTurn;
       NextTurn = NowTurn;
       NowTurn = tmpTurn;
     }
-	}
+  }
 }
